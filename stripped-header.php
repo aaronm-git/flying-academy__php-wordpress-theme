86fb6717f4cc5122714df7e907eb69225f7f7325
wilfredopadron <38760237+wilfredopadron@users.noreply.github.com>
2018-06-13 13:14:29 -0400

    pure css header
    all css
    bug fixes

---
+++ stripped-header.php
@@ -0,0 +1,182 @@
+			<header class="main-header">
+				<ul class="left-header">
+					<li class="hamburger">
+						<input id="hamburger" type="checkbox">
+						<label for="hamburger"></label>
+						<ul class="primary-nav">
+							<li class="mega-menu">
+								<input id="courses" type="checkbox">
+								<label for="courses">Courses</label>
+								<a>Courses</a>		
+								<ul>
+									<li>
+										<input id="faa-programs" type="checkbox">
+										<label for="faa-programs">FAA Programs</label>
+										<a>FAA Programs</a>
+										<ul>
+											<li><a href="course-single.php">0-1500</a></li>
+											<li><a href="course-single.php">0-CPL</a></li>
+											<li><a href="course-single.php">0-ATPL (USA)</a></li>
+										</ul>
+									</li>
+									<li>
+										<input id="faa-modules" type="checkbox">
+										<label for="faa-modules">FAA Modules</label>
+										<a>FAA Modules</a>
+										<ul>
+											<li><a href="course-single.php">CFI</a></li>
+											<li><a href="course-single.php">CFII</a></li>
+											<li><a href="course-single.php">CPL</a></li>
+											<li><a href="course-single.php">MEP</a></li>
+											<li><a href="course-single.php">PPL</a></li>
+											<li><a href="course-single.php">IR</a></li>
+										</ul>
+									</li>
+									<li>
+										<input id="easa-programs" type="checkbox">
+										<label for="easa-programs">EASA Programs</label>
+										<a>EASA Programs</a>
+										<ul>
+											<li><a href="course-single.php">0-1500</a></li>
+											<li><a href="course-single.php">0-CPL</a></li>
+											<li><a href="course-single.php">0-ATPL</a></li>
+											<li><a href="course-single.php">ATPL Integrated</a></li>
+										</ul>
+									</li>
+									<li>
+										<input id="easa-modules" type="checkbox">
+										<label for="easa-modules">EASA Modules</label>
+										<a>EASA Modules</a>
+										<ul>
+											<li><a href="course-single.php">Aviation English</a></li>
+											<li><a href="course-single.php">ATPL Theory</a></li>
+											<li><a href="course-single.php">CPL</a></li>
+											<li><a href="course-single.php">CPL THeory</a></li>
+											<li><a href="course-single.php">FI</a></li>
+											<li><a href="course-single.php">IRI</a></li>
+											<li><a href="course-single.php">JOC</a></li>
+											<li><a href="course-single.php">MCC</a></li>
+											<li><a href="course-single.php">MEP</a></li>
+											<li><a href="course-single.php">Night Rating</a></li>
+											<li><a href="course-single.php">IR</a></li>
+										</ul>
+									</li>								
+								</ul>
+							</li>
+							<li class="mega-menu">
+								<input id="locations" type="checkbox">
+								<label for="locations">Locations</label>
+								<a>Locations</a>	
+								<ul>
+									<li>
+										<input id="los-angeles" type="checkbox">
+										<label for="los-angeles">Los Angeles</label>
+										<a>Los Angeles</a>									
+										<ul>
+											<li><a href="location-single.php">Accomodations</a></li>
+											<li><a href="location-courses.php">Courses</a></li>
+											<li><a href="time-building.php">Time Building</a></li>
+											<li><a href="location-contacts.php">Contact Local</a></li>
+										</ul>
+									</li>
+									<li>
+										<input id="miami" type="checkbox">
+										<label for="miami">Miami</label>
+										<a>Miami</a>									
+										<ul>
+											<li><a href="location-single.php">Accomodations</a></li>
+											<li><a href="location-courses.php">Courses</a></li>
+											<li><a href="time-building.php">Time Building</a></li>
+											<li><a href="location-contacts.php">Contact Local</a></li>
+										</ul>
+									</li>
+									<li>
+										<input id="prague" type="checkbox">
+										<label for="prague">Prague</label>
+										<a>Prague</a>									
+										<ul>
+											<li><a href="location-single.php">Accomodations</a></li>
+											<li><a href="location-courses.php">Courses</a></li>
+											<li><a href="time-building.php">Time Building</a></li>
+											<li><a href="location-contacts.php">Contact Local</a></li>
+										</ul>
+									</li>
+									<li>
+										<input id="brno" type="checkbox">
+										<label for="brno">Brno</label>
+										<a>Brno</a>							
+										<ul>
+											<li><a href="location-single.php">Accomodations</a></li>
+											<li><a href="location-courses.php">Courses</a></li>
+											<li><a href="time-building.php">Time Building</a></li>
+											<li><a href="location-contacts.php">Contact Local</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>							
+							<li><a href="index.php">Home</a></li>
+							<li>
+								<input id="company" type="checkbox">
+								<label for="company">Company Information</label>
+								<a>Company Information</a>
+								<ul>
+									<li><a>About Us</a></li>
+									<li><a>Business to Business</a></li>
+								</ul>
+							</li>
+							<li>
+								<input id="time-building" type="checkbox">
+								<label for="time-building">Time Building</label>
+								<a>Time Building</a>
+								<ul>
+									<li><a href="time-building.php">Los Angeles</a></li>
+									<li><a href="time-building.php">Miami</a></li>
+									<li><a href="time-building.php">Prague</a></li>
+									<li><a href="time-building.php">Brno</a></li>
+								</ul>
+							</li>
+							<li><a>Fleet</a></li>
+							<li><a>Fees</a></li>
+							<li><a>Blog</a></li>
+							<li><a>Contact Us</a></li>
+						</ul>
+					</li>
+					<li class="logo"><a href="index.php"><img src="images/logo.png"></a></li>
+					<li>
+						<a>Company</a>
+						<ul>
+							<li><a>About Us</a></li>
+							<li><a>Business to Business</a></li>
+						</ul>
+					</li>
+					<li><a>Courses</a></li>
+					<li><a>Locations</a></li>
+					<li><a>Blog</a></li>
+				</ul>
+				<ul class="right-header">
+					<li class="search">
+						<input id="search" type="checkbox">
+						<label for="search"><i class="fas fa-search"></i></label>
+						<form>
+							<input type="text" name="keywords" value="Enter Keywords">
+							<input type="submit" name="submit" value="&#xf002;">
+						</form>
+					</li>
+					<li><a><i class="fas fa-envelope"></i></a></li>
+					<li><a><i class="fas fa-phone"></i></a></li>
+					<li class="language">
+						<input id="language" type="checkbox">
+						<label for="language">Language</label>
+						<a>Language</a>
+						<ul>
+							<li><a lang="en" style="background-image: url(images/flags/USA.svg);">English</a></li>
+							<li><a lang="cs" style="background-image: url(images/flags/Czech Republic.svg);">Česká</a></li>
+							<li><a lang="it" style="background-image: url(images/flags/Italy.svg);">Italiano</a></li>
+							<li><a lang="ru" style="background-image: url(images/flags/Russian Federation.svg);">Русский</a></li>
+							<li><a lang="zh" style="background-image: url(images/flags/China.svg);">简体中文</a></li>
+							<li><a lang="es" style="background-image: url(images/flags/Spain.svg);">Español</a></li>
+						</ul>
+					</li>
+					<li id="contact"><a>Contact Us</a></li>
+				</ul>
+			</header>
